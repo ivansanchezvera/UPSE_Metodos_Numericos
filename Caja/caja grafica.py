cdaf56193b70ca77accce7bfe2c43a9b993afa06
Ivan <ivansanchez_1988@hotmail.com>
2016-04-27 12:32:52 -0500

    Caja: Primer Ejercicio con metodos numericos para resolver problema de volumen de caja con lamina

---
+++ Caja/caja grafica.py
@@ -0,0 +1,11 @@
+#Creado en base a:
+#Rodriguez Ojeda, L. (2015). Analisis Numerico Basico. Guayaquil: ESCUELA POLITECNICA DEL LITORAL (ESPOL).
+
+from pylab import*
+#Arange devuelve un arreglo con numeros desde el elemento de inicio (primer parametro)
+#hasta el elemento final (segundo parametro) avanzando segun el 3er parametro
+x = arange(0,20,0.1)
+f = 250-(192*x) + (28*x**2) - x**3
+plot(x,f)
+grid(True)
+show()
